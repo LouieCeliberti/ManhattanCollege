9068114a19d4f05ef708df4cae09f53ec072a643
LouieCeliberti <Louismceliberti@gmail.com>
2022-09-19 21:21:26 -0400

    Update CMPT439_HW2_Secant.py

--- CMPT439_HW2_Secant.py
+++ CMPT439_HW2_Secant.py
@@ -1,56 +1,56 @@
 import math
 
-def func(x):
-    return ((2*math.sin(x)-math.exp(x))/4)-1
+def f(x):
+    return 2*math.sin(x)-(math.exp(x)/4)-1
 
 tol = 0.000001
-def Secant(x0, x1, tol, error_type):
+def Secant(x1, x2, tol, f, flag):
 
-    if abs(func(x0)) > abs(func(x1)):
-        xbuf = x0
-        x0 = x1
-        x1 = xbuf
+    if abs(f(x1)) > abs(f(x2)):
+        xswap = x1
+        x1 = x2
+        x2 = xswap
  
     iter = 0; error = 100
 
     while error >= tol:
-        x2 = x1-func(x1)*((x0-x1)/(func(x0)-func(x1)))
+        x3 = x2-f(x2)*((x1-x2)/(f(x1)-f(x2)))
         iter += 1
 
-        if func(x2) == 0:
-            print('The root is: ', x2)
+        if f(x1) == 0 and f(x3) == 0:
+            print('The root is: ', x3)
             print('Iterations: ', iter)
             break
         else:
-            x0 = x1
             x1 = x2
+            x2 = x3
+        
+        if flag == 1:
+            error = abs(x1 - x2)
+        elif flag == 2:
+            error = abs((x1 - x2) / x2)
+        elif flag == 3:
+            error = abs(f(x2))
 
-        if error_type == 1:
-            error = abs(x0 - x1)
-        elif error_type == 2:
-            error = abs((x0 - x1) / x1)
-        elif error_type == 3:
-            error = abs(func(x1))
-
-    print('Root is : %f' % x2)
+    print('Root is : %f' % x3)
     print('Iteration: %f' % iter )
     print('Error: ', error)
 
 #Itervals for question 2
-x0 = -7; x1 = -5
+x1 = -7; x2 = -5
 y1 = -5; y2 = -3
 
 print('\nAbsolute Approximate Error: ')
-Secant(x0, x1, tol, 1)
-Secant(y1, y2, tol, 1)
+Secant(x1, x2, tol, f, 1)
+Secant(y1, y2, tol, f,  1)
 print('\n')
 
 print('Absolute Relative Approximate Error: ')
-Secant(x0, x1, tol, 2)
-Secant(y1, y2, tol, 2)
+Secant(x1, x2, tol, f, 2)
+Secant(y1, y2, tol, f, 2)
 print('\n')
 
 print('True Absolute Error:')
-Secant(x0, x1, tol, 3)
-Secant(y1, y2, tol, 3)
+Secant(x1, x2, tol, f, 3)
+Secant(y1, y2, tol, f, 3)
 print('\n')
